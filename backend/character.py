eaa9943f3f7b5a6e8209c009e352a7a7151188f3
Travis Smith <tsmit864@wgu.edu>
2026-09-05 16:50:25 -0400

    changes variables in character.py

--- backend/character.py
+++ backend/character.py
@@ -1,19 +1,23 @@
 import requests
 import json
 import random
+import time
+import redis
 
 class Character: 
+    character_url = "https://rickandmortyapi.com/api/character/"
+
     def __init__(self):
         pass
 
         # This function generates a random number between 1 and the total number of characters in the Rick and Morty API
     def _gen_random_number(self):
-        totalCharacters = requests.get("https://rickandmortyapi.com/api/character").json()['info']['count']
+        totalCharacters = requests.get(self.character_url).json()['info']['count']
         return random.randint(1, totalCharacters)
 
         # This function fetches a random character from the Rick and Morty API and prints the character's data in a formatted JSON structure
     def get_random_character(self):
-        data = requests.get(f"https://rickandmortyapi.com/api/character/{self._gen_random_number()}").json()
+        data = requests.get(f"{self.character_url}{self._gen_random_number()}").json()
         return{
             "id": data['id'],
             "name": data['name'],
